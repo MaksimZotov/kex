936021597e2096591f7283d0a47f3288362b0954
MaksimZotov <zotovmaksim1254@gmail.com>
2020-05-12 09:58:11 +0300

    made reading annotations for parameters through the word "annotation"

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/annotations/ExternalAnnotationsLoader.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/annotations/ExternalAnnotationsLoader.kt
@@ -94,31 +94,21 @@ class ExternalAnnotationsLoader : AnnotationsLoader {
     override fun getExactCall(name: String, vararg params: String): AnnotatedCall? = getExactCallPrivate(name, *params)
 
     private fun parseParamTypes(paramsStr: String) =
-        when {
-            paramsStr.isBlank() -> emptyList()
-            else -> paramsStr.split(',').map { transformTypeName(it.trim()) }
-        }
+            when {
+                paramsStr.isBlank() -> emptyList()
+                else -> paramsStr.split(',').map { transformTypeName(it.trim()) }
+            }
 
     private fun transformTypeName(name: String) = when (name) {
         "boolean" -> "bool"
         else -> name.replace('.', '/')
-            .replace("///", "...")
-            .replace(" ", "")
+                .replace("///", "...")
+                .replace(" ", "")
     }
 
-    fun parseParamsAnnotations(node: Element): Map<String, List<AnnotationInfo>> {
-        val indexAndAnnotatons = mutableMapOf<String, List<AnnotationInfo>>()
-        val params = node.getElementsByTagName("parameter")
-        var i = 0
-        for (param in generateSequence { if (i == params.length) null else params.item(i++) as Element })
-            indexAndAnnotatons[param.attributes.getNamedItem("index").nodeValue] =
-                    parseAnnotations(param, "annotationParam")
-        return indexAndAnnotatons
-    }
-
-    fun parseAnnotations(param: Element, annotationType: String): List<AnnotationInfo> {
+    fun parseAnnotations(param: Element): List<AnnotationInfo> {
         val result = mutableListOf<AnnotationInfo>()
-        val nodes = param.getElementsByTagName(annotationType)
+        val nodes = param.getElementsByTagName("annotation")
         var i = 0
         for (annotationNode in generateSequence { if (i == nodes.length) null else nodes.item(i++) as Element }) {
             val name = annotationNode.attributes.getNamedItem("name").nodeValue
@@ -128,107 +118,113 @@ class ExternalAnnotationsLoader : AnnotationsLoader {
                 val paramNode = paramNodes.item(j)
                 val attributes = paramNode.attributes
                 args[attributes.getNamedItem("name")?.nodeValue ?: "value"] =
-                    attributes.getNamedItem("val")?.nodeValue ?: paramNode.textContent.trim()
+                        attributes.getNamedItem("val")?.nodeValue ?: paramNode.textContent.trim()
             }
             result += AnnotationManager.build(name, args) ?: continue
         }
         return result
     }
 
-    private fun loadAnnotations(stream: InputStream, file: String) {
-        try {
-            val xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream)
-            xmlDoc.documentElement.normalize()
-            check(xmlDoc.documentElement.nodeName == "root") {
-                "Annotations xml document root name is not \"root\"," +
-                        "actual name is \"${xmlDoc.documentElement.nodeName}\""
+    private fun loadAnnotations(stream: InputStream, file: String) = try {
+        val xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream)
+        xmlDoc.documentElement.normalize()
+        check(xmlDoc.documentElement.nodeName == "root") {
+            "Annotations xml document root name is not \"root\"," +
+                    "actual name is \"${xmlDoc.documentElement.nodeName}\""
+        }
+        val nodes = xmlDoc.getElementsByTagName("item")
+        var i = 0
+        for (node in generateSequence { if (i == nodes.length) null else nodes.item(i++) }) {
+            val nameAttr = node.attributes.getNamedItem("name").nodeValue.trim()
+            var delimiter = nameAttr.indexOf(' ')
+            if (delimiter < 0)
+            // this is not a call
+                continue
+            val packageName = nameAttr.substring(0 until delimiter).replace('.', '/')
+            while (nameAttr[delimiter] == ' ') delimiter++
+            var j = nameAttr.indexOfAny(charArrayOf(' ', '('), delimiter)
+            var returnType = nameAttr.substring(delimiter until j)
+            val callName: String
+            if (nameAttr[j] == '(') {
+                callName = "<init>"
+                returnType = packageName
+            } else {
+                delimiter = j + 1
+                while (nameAttr[delimiter] == ' ') delimiter++
+                j = nameAttr.indexOfAny(charArrayOf('('), delimiter)
+                callName = nameAttr.substring(delimiter until j)
             }
-            val nodes = xmlDoc.getElementsByTagName("item")
+            returnType = transformTypeName(returnType)
+            ktassert(nameAttr[j] == '(')
+            delimiter = j + 1
+            while (nameAttr[delimiter] == ' ') delimiter++
+            j = nameAttr.indexOf(')', delimiter)
+            ktassert(j > 0)
+            val params = parseParamTypes(nameAttr.substring(delimiter until j))
+            val call = emplaceCall(packageName, callName, returnType, params)
+            delimiter = j + 1
+            while (nameAttr.length > delimiter && nameAttr[delimiter] == ' ') delimiter++
+            if (delimiter == nameAttr.length) delimiter--
+
+            val annotations = try { parseAnnotations(node as Element) }
+            catch(thr: Throwable) { throw AnnotationParserException("Error while parsing annotations for \"$call\"", thr) }
+
+            val paramsAnnotations = mutableMapOf<String, List<AnnotationInfo>>()
+            val parameteres = node.getElementsByTagName("parameter")
             var i = 0
-            for (node in generateSequence { if (i == nodes.length) null else nodes.item(i++) }) {
-                val nameAttr = node.attributes.getNamedItem("name").nodeValue.trim()
-                var delimiter = nameAttr.indexOf(' ')
-                if (delimiter < 0)
-                // this is not a call
-                    continue
-                val packageName = nameAttr.substring(0 until delimiter).replace('.', '/')
-                while (nameAttr[delimiter] == ' ') delimiter++
-                var j = nameAttr.indexOfAny(charArrayOf(' ', '('), delimiter)
-                var returnType = nameAttr.substring(delimiter until j)
-                val callName: String
-                if (nameAttr[j] == '(') {
-                    callName = "<init>"
-                    returnType = packageName
-                } else {
-                    delimiter = j + 1
-                    while (nameAttr[delimiter] == ' ') delimiter++
-                    j = nameAttr.indexOfAny(charArrayOf('('), delimiter)
-                    callName = nameAttr.substring(delimiter until j)
+            try {
+                for (param in generateSequence { if (i == parameteres.length) null else parameteres.item(i++) as Element })
+                    paramsAnnotations[param.attributes.getNamedItem("index").nodeValue] =
+                            parseAnnotations(param)
+            } catch(thr: Throwable) {
+                throw AnnotationParserException("Error while parsing annotations for \"$call\"", thr)
+            }
+
+            annotations.forEach { it.mutableCall = call }
+
+            // this is params
+            for ((index, annotations) in paramsAnnotations) {
+                val annotationsParam = call.params[index.toInt()].annotations
+                annotationsParam += annotations
+                annotationsParam.forEach {
+                    try { it.initialize(i) }
+                    catch (thr: Throwable) {
+                        throw AnnotationParserException("Error while initializing an annotation functionality" +
+                                " instance $it for parameter #$i of $call", thr)
+                    }
                 }
-                returnType = transformTypeName(returnType)
-                ktassert(nameAttr[j] == '(')
-                delimiter = j + 1
-                while (nameAttr[delimiter] == ' ') delimiter++
-                j = nameAttr.indexOf(')', delimiter)
-                ktassert(j > 0)
-                val params = parseParamTypes(nameAttr.substring(delimiter until j))
-                val call = emplaceCall(packageName, callName, returnType, params)
-                delimiter = j + 1
-                while (nameAttr.length > delimiter && nameAttr[delimiter] == ' ') delimiter++
-                if (delimiter == nameAttr.length) delimiter--
+            }
 
-                val annotations = try { parseAnnotations(node as Element, "annotation") }
-                catch(thr: Throwable) { throw AnnotationParserException("Error while parsing annotations for \"$call\"", thr) }
-
-                val paramsAnnotations = try { parseParamsAnnotations(node) }
-                catch(thr: Throwable) { throw AnnotationParserException("Error while parsing annotations for \"$call\"", thr) }
-
-                annotations.forEach { it.mutableCall = call }
-
-                // this is params
-                for ((index, annotations) in paramsAnnotations) {
-                    val annotationsParam = call.params[index.toInt()].annotations
-                    annotationsParam += annotations
-                    annotationsParam.forEach {
-                        try { it.initialize(i) }
+            when (nameAttr[delimiter]) {
+                in '0'..'9' -> {
+                    // this is a parameter
+                    val n = nameAttr.substring(delimiter).toInt()
+                    val param = call.params[n]
+                    param.annotations += annotations
+                    annotations.forEach {
+                        try { it.initialize(n) }
                         catch (thr: Throwable) {
                             throw AnnotationParserException("Error while initializing an annotation functionality" +
-                                    " instance $it for parameter #$i of $call", thr)
+                                    " instance $it for parameter #$n of $call", thr)
                         }
                     }
                 }
-
-                when (nameAttr[delimiter]) {
-                    in '0'..'9' -> {
-                        // this is a parameter
-                        val n = nameAttr.substring(delimiter).toInt()
-                        val param = call.params[n]
-                        param.annotations += annotations
-                        annotations.forEach {
-                            try { it.initialize(n) }
-                            catch (thr: Throwable) {
-                                throw AnnotationParserException("Error while initializing an annotation functionality" +
-                                        " instance $it for parameter #$n of $call", thr)
-                            }
-                        }
-                    }
-                    ')' -> {
-                        // this is a call
-                        val annotationsCall = annotations.filter { !it.name.contains("Param") }
-                        call.annotations += annotationsCall
-                        annotationsCall.forEach {
-                            try { it.initialize(-1) }
-                            catch (thr: Throwable) {
-                                throw AnnotationParserException("Error while initializing an annotation functionality" +
-                                        " instance $it for $call", thr)
-                            } }
-                    }
-                    else -> throw IllegalStateException("Name attribute of the call has invalid format")
+                ')' -> {
+                    // this is a call
+                    val annotationsCall = annotations.filter { !it.name.contains("Param") }
+                    call.annotations += annotationsCall
+                    annotationsCall.forEach {
+                        try { it.initialize(-1) }
+                        catch (thr: Throwable) {
+                            throw AnnotationParserException("Error while initializing an annotation functionality" +
+                                    " instance $it for $call", thr)
+                        } }
                 }
+                else -> throw IllegalStateException("Name attribute of the call has invalid format")
             }
-        } catch (thr: Throwable) {
-            throw AnnotationParserException("Error while parsing \"$file\"", thr)
         }
+    } catch (thr: Throwable) {
+        throw AnnotationParserException("Error while parsing \"$file\"", thr)
     }
 
     override fun toString(): String {
